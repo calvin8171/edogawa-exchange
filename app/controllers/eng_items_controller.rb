c2ff0783da3c75cae5158521a7b6c22b8aea6ea4
calvin8171 <calvin8171@gmail.com>
2022-05-20 17:23:24 +0900

    eng show, edit & delete first commit

--- app/controllers/eng_items_controller.rb
+++ app/controllers/eng_items_controller.rb
@@ -1,5 +1,10 @@
 class EngItemsController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
+    @eng_items = EngItem.all.order(created_at: :desc)  
   end
 
   def new
@@ -15,9 +20,39 @@ class EngItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @eng_item.update(eng_item_params)
+      redirect_to eng_item_path(@eng_item)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @eng_item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
   
   def eng_item_params
     params.require(:eng_item).permit(:image, :item_name, :description, :category_id, :condition_id, :area_id, :price).merge(user_id: current_user.id)
+  end
+
+  def find_params
+    @eng_item = EngItem.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @eng_item.user
   end
 end
--- app/models/eng_item.rb
+++ app/models/eng_item.rb
@@ -9,7 +9,8 @@ class EngItem < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "Please select one" }  
-  validates :condition_id, numericality: { other_than: 1 , message: "Please select one" }  
-  validates :area_id, numericality: { other_than: 1 , message: "Please select one" } 
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }  
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }  
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :price, presence: true
 end
